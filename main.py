4e7e1a2cf48e7fab236f8e20df06b69e366c5f13
Benjamin Eldridge <beldridge@unc.edu>
2022-11-16 22:27:51 -0500

    main update

--- main.py
+++ main.py
@@ -28,57 +28,62 @@ pygame.time.set_timer(ADDENEMY, TIME_BETWEEN_ENEMIES, NUM_OF_ENEMIES)
 
 clock = pygame.time.Clock()
 
-running = True
 
-# Main loop
-while running:
-    # Event queue
-    for event in pygame.event.get():
-        # Stop loop when X button is hit
-        if event.type == QUIT:
-            running = False
-        # Check for KEYDOWN events
-        elif event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
+
+def main():
+    running = True
+    # Main loop
+    while running:
+        # Event queue
+        for event in pygame.event.get():
+            # Stop loop when X button is hit
+            if event.type == QUIT:
                 running = False
-        elif event.type == ADDENEMY:
-            # Create a new enemy
-            # new_enemy = LeftFlyingEnemy()
-            new_enemy = SeekingEnemy()
-            enemies.add(new_enemy)
-            all_sprites.add(new_enemy)
+            # Check for KEYDOWN events
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+            elif event.type == ADDENEMY:
+                # Create a new enemy
+                # new_enemy = LeftFlyingEnemy()
+                new_enemy = SeekingEnemy()
+                enemies.add(new_enemy)
+                all_sprites.add(new_enemy)
 
-    # Initalize a dict storing all pressed keys
-    pressed_keys = pygame.key.get_pressed()
-    
-    # Call the update method on player checking all pressed keys for True/False
-    player.update(pressed_keys)
+        # Initalize a dict storing all pressed keys
+        pressed_keys = pygame.key.get_pressed()
+        
+        # Call the update method on player checking all pressed keys for True/False
+        player.update(pressed_keys)
 
-    # for i in enemies:
-    #     other_enemies = pygame.sprite.Group()
-    #     other_enemies.add(enemies)
-    #     other_enemies.remove(i)
-    #     if pygame.sprite.spritecollideany(i, other_enemies):
-    #         i.update_collision(player, enemies)
-    #     else:
-    #         i.update(player, enemies)
+        # for i in enemies:
+        #     other_enemies = pygame.sprite.Group()
+        #     other_enemies.add(enemies)
+        #     other_enemies.remove(i)
+        #     if pygame.sprite.spritecollideany(i, other_enemies):
+        #         i.update_collision(player, enemies)
+        #     else:
+        #         i.update(player, enemies)
 
-    # Updates enemy positions
-    enemies.update(player, enemies)
+        # Updates enemy positions
+        enemies.update(player, enemies)
 
-    # Fill the background with black
-    screen.fill((0, 0, 0))
+        # Fill the background with black
+        screen.fill((0, 0, 0))
 
-    for entity in all_sprites:
-        screen.blit(entity.surf, entity.rect)
+        for entity in all_sprites:
+            screen.blit(entity.surf, entity.rect)
 
-    # Check for any collisions between the player and any enemy
-    if pygame.sprite.spritecollideany(player, enemies):
-        # If so, kill player and end game (end loop)
-        player.kill()
-        running = False
+        # Check for any collisions between the player and any enemy
+        if pygame.sprite.spritecollideany(player, enemies):
+            # If so, kill player and end game (end loop)
+            player.kill()
+            running = False
 
-    # Update the display
-    pygame.display.flip()
+        # Update the display
+        pygame.display.flip()
 
-    clock.tick(TICK_RATE)
+        clock.tick(TICK_RATE)
+
+if __name__ == '__main__':
+    main()
